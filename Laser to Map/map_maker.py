06c1146b0d546c4d798b45b3e1a3cbf7f56182da
Aravindhakumar M <65991985+Aravindhakumar-M@users.noreply.github.com>
2023-10-28 22:02:52 +0530

    Update and rename map_saver.py to Laser to Map/map_maker.py

--- Laser to Map/map_maker.py
+++ Laser to Map/map_maker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 import math
@@ -10,7 +10,10 @@ from tf.transformations import euler_from_quaternion
 
 class MapMaker:
     def __init__(self):
+        # Initialize ROS node
         rospy.init_node('map_pub', anonymous=True)
+        
+        # Initialize variables and data structures
         self.scan_data = None
         self.odom_data = None
         self.map_resolution = 0.05
@@ -18,6 +21,8 @@ class MapMaker:
         self.map_height = 800
         self.previous_map_data = np.zeros((self.map_height, self.map_width), dtype=np.int8)
         self.map_data = np.zeros((self.map_height, self.map_width), dtype=np.int8)
+        
+        # Create an OccupancyGrid message to represent the map
         self.map_msg = OccupancyGrid()
         self.map_msg.header.frame_id = 'map'
         self.map_msg.info.resolution = self.map_resolution
@@ -26,6 +31,8 @@ class MapMaker:
         self.map_msg.info.origin.position.x = -self.map_width * self.map_resolution / 2
         self.map_msg.info.origin.position.y = -self.map_height * self.map_resolution / 2
         self.map_msg.info.origin.orientation.w = 1.0
+
+        # Initialize path and set up publishers and subscribers
         self.path = []
         self.map_pub = rospy.Publisher('/map', OccupancyGrid, queue_size=10)
         self.path_pub = rospy.Publisher('/path', PoseStamped, queue_size=10)
@@ -34,6 +41,7 @@ class MapMaker:
         rospy.spin()
 
     def scan_callback(self, scan_msg):
+        # Callback for handling laser scan data
         self.scan_data = scan_msg
 
         if self.odom_data is not None:
@@ -42,7 +50,8 @@ class MapMaker:
             orientation_q = self.odom_data.pose.pose.orientation
             _, _, yaw = euler_from_quaternion([orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w])
             current_map_data = np.copy(self.previous_map_data)
-            for i in range(len(self.scan_data.ranges)):
+
+            for i in range(len(self.scan_data.ranges):
                 if self.scan_data.ranges[i] < self.scan_data.range_max:
                     angle = self.scan_data.angle_min + i * self.scan_data.angle_increment + yaw
                     x_range = int((x + self.scan_data.ranges[i] * math.cos(angle) - self.map_msg.info.origin.position.x) / self.map_resolution)
@@ -60,6 +69,7 @@ class MapMaker:
             self.map_pub.publish(self.map_msg)
 
     def odom_callback(self, odom_msg):
+        # Callback for handling odometry data
         self.odom_data = odom_msg
 
         if self.scan_data is not None:
@@ -79,11 +89,11 @@ class MapMaker:
             self.path.append(pose)
 
             # Publish the path
-            # path_msg = Path()
-            # path_msg.header.stamp = rospy.Time.now()
-            # path_msg.header.frame_id = 'map'
-            # path_msg.poses = self.path
-            # self.path_pub.publish(path_msg)
+            path_msg = Path()
+            path_msg.header.stamp = rospy.Time.now()
+            path_msg.header.frame_id = 'map'
+            path_msg.poses = self.path
+            self.path_pub.publish(path_msg)
 
 if __name__ == '__main__':
     MapMaker()
