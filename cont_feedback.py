928e8387e6042f022d1c6a8a0c6cd0e6a8d5fd6e
Aravindhakumar M <65991985+Aravindhakumar-M@users.noreply.github.com>
2023-10-28 14:35:48 +0530

    Update cont_feedback.py

--- cont_feedback.py
+++ cont_feedback.py
@@ -1,55 +1,43 @@
 #!/usr/bin/env python3
-
 import rospy
-from std_msgs.msg import String, Float64
-import math
-import numpy as np
-from nav_msgs.msg import OccupancyGrid, Path
-from geometry_msgs.msg import Twist
+from std_msgs.msg import Int16MultiArray
+import curses
 
-def py_node(msg):
-    
-    path = [(pose.pose.position.x, pose.pose.position.y) for pose in msg.poses]
-    pose = msg.poses[0].pose
-    
-    max_linear_vel = 0.5
-    max_angular_vel = 1.0
+# Initialize the curses library for capturing keyboard input
+screen = curses.initscr()
+curses.noecho()
+curses.cbreak()
+screen.keypad(True)
+direction = Int16MultiArray()
 
-    distance = ((pose.position.x**2) + (pose.position.y**2)) ** 0.5
-    angle = math.atan2(pose.position.y, pose.position.x)
-    angular_vel = angle * 0.5
-    linear_vel = min(distance, max_linear_vel)
-    angular_vel = max(min(angular_vel, max_angular_vel), -max_angular_vel)
-    
-    x=linear_vel
-    y=angular_vel
+def teleop_node():
+    # Capture a character from the keyboard
+    char = screen.getch()
+    if char == curses.KEY_RIGHT:
+        direction = [0, 0, 1, 0]  # Move right
+    elif char == curses.KEY_LEFT:
+        direction = [0, 0, 0, 1]  # Move left
+    elif char == curses.KEY_UP:
+        direction = [1, 0, 0, 0]  # Move forward
+    elif char == curses.KEY_DOWN:
+        direction = [0, 1, 0, 0]  # Move backward
 
-    x1=(2*x+2.2*y)/2
-    x2=(2*x-2.2*y)/2
+    # Initialize the ROS node and publish the direction as Int16MultiArray
+    pub = rospy.Publisher("/wheel", Int16MultiArray, queue_size=10)
+    rospy.init_node('teleop_node', anonymous=True)
     
-    cmdvel(x1,x2)
-    
-def cmdvel(x1,x2):
+    # End curses input handling to return to normal terminal behavior
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
 
-    pub1 = rospy.Publisher('/cont1/command', Float64, queue_size=10)
-    pub2 = rospy.Publisher('/cont2/command', Float64, queue_size=10)
-    pub3 = rospy.Publisher('/cont3/command', Float64, queue_size=10)
-    pub4 = rospy.Publisher('/cont4/command', Float64, queue_size=10)
-    
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown():
-        pub1.publish(x1)
-        pub2.publish(x1)
-        pub3.publish(x2)
-        pub4.publish(x2)
-        rate.sleep()
+    # Publish the direction command
+    pub.publish(data=direction)
+    print(direction)
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('cont_node', anonymous=True)
-        print("Started Velocity Publisher node")
-        rospy.Subscriber('path', Path, py_node)
-        rospy.spin()
+        while not rospy.is_shutdown():
+            teleop_node()
     except rospy.ROSInterruptException:
         pass
